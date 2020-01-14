c4ada471cec1e3da20ef17de4707af24ecd4d105
andar <vitaliy.for@gmail.com>
2020-01-14 22:22:51 +0200

    - refactor routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 
 require('dotenv').config();
 
-const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
